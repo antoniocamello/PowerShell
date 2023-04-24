0aca08d15eff1b20c967b4a9ffe81a9000e804d8
Antonio Camelo <113397323+antoniocamello@users.noreply.github.com>
2023-04-24 12:04:01 -0300

    Add files via upload

---
+++ Aplica-Hardening.ps1
@@ -0,0 +1,48 @@
+﻿<# 
+NOME:
+    Aplica-Hardening
+DESCRIÇÃO:
+    Este script instala o Hardening V1 no Datasul
+EXEMPLO:hardening-Invoker-OS
+    PS C:\totvs\hardening .\Aplica-Hardening.ps1
+AUTORES:
+    Name: Antonio Camelo
+	E-mail: antonio.camelo@totvs.com.br
+    
+    Nome: Leandro Costa
+    E-mail: leandro.costa@totvs.com
+#>
+
+Clear-Host
+# Verifica se o diretório existe, caso não exista, cria o diretório
+if (!(Test-Path "C:\totvs\hardening")) {
+    New-Item -ItemType Directory -Path "C:\totvs\hardening"
+    Write-Host "Diretório 'hardening' criado com sucesso!"
+}
+
+# Faz o download do arquivo 'hardening-Invoker-OS'
+
+$fileUrl = "http://192.168.1.125/hardening/datasul/hardening_os/1.0/hardening-Invoker-OS.ps1"
+$filePath = "C:\totvs\hardening\hardening-Invoker-OS.ps1"
+Invoke-WebRequest -Uri $fileUrl -OutFile $filePath
+$file = Get-Item -Path $filePath
+
+#Calcula o hash do arquivo usando o algoritmo SHA256
+$hash = Get-FileHash -Path $file.FullName -Algorithm SHA256
+
+#Compara o hash calculado com o hash conhecido do script hardening-Invoker-OS
+if ($hash.Hash -eq "AEC1D24EDAB7D2D2CFA0253AFC1FF5FC09D3643A1C913E3A8B957E91E8B66D88") {
+    
+    # Se o hash for válido, executa o script
+    .\newhardening.ps1 -apply hardening -configfile "c:\totvs\cis_hardening_windows_server_2016_default_v0.4.json"
+} else {
+    # Casp O hash seja inválido, imprima uma mensagem de erro e saia do script
+    Write-Host "O hash do arquivo baixado é inválido. O download pode ter sido comprometido. Não é seguro continuar com a execução do script." -ForegroundColor Red
+    Exit 1
+}
+
+    #Cria Schedule task
+    #Gerar arquivo de log da execução do script com timestamp das ações
+    #Restart
+    #Verificar o caminho do diretório padrão c:\totvs\.
+    #>
