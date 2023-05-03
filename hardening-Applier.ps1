321e915fb6e4dae2a0f3cf11e496243830d640eb
Antonio F Camelo <113397323+antoniocamello@users.noreply.github.com>
2023-05-03 16:14:59 -0300

    Update hardening-Applier.ps1

--- hardening-Applier.ps1
+++ hardening-Applier.ps1
@@ -12,32 +12,6 @@ EXEMPLO:hardening-Invoker-OS
 
 Clear-Host
 
-param (
-	
-    [parameter(mandatory = $true, HelpMessage="generic para aplicar o hardening de SegInfo." )]
-    [string] $product = $null,
-    [parameter(mandatory = $true, HelpMessage="stable para pegar a versão estável do aquivo de configuração do hardening.")]
-    [string] $version = $null 
-  )
-
-  if (-not (Test-Path $configDir)) {
-    Write-Error "O diretório $configDir não existe."
-    return
-}
-
-$validProducts = @("generic", "datasul", "legaldesk", "sisjuri", "protheus", "rm", "smartrm", "Winthor", "Consinco")
-if (-not ($validProducts -contains $product)) {
-    Write-Error "O produto '$product' não é válido. Os produtos válidos são: $($validProducts -join ", ")."
-    return
-}
-
-$validVersions = @("stable", "latest")
-if (-not ($validVersions -contains $version)) {
-    Write-Error "A versão '$version' não é válida. As versões válidas são: $($validVersions -join ", ")."
-    return
-}
-
-
 #Função que verifica a execução como Admin
 function Admin-check {
     $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -56,157 +30,101 @@ if (!(Test-Path $localDirectory)) {
 }
 
 #Função que Cria o dicionário de dados com os arquivos de configuração dos produtos
-function arquivos-hardening($product, $version) {
+function download_hardening_files()
+ {
+
+     Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $product,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $version
+    )
+
+
     # Define a URL do repositório
-    if ($product -eq "generic") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
-    }
-    elseif ($product -eq "datasul") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/datasul/$version"
-    }
-    elseif ($product -eq "legaldesk") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/legaldesk/$version"
-    }
-    elseif ($product -eq "sisjuri") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/sisjuri/$version"
-    }
-    elseif ($product -eq "protheus") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/protheus/$version"
-    }
-    elseif ($product -eq "rm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/rm/$version"
-    }
-    elseif ($product -eq "smartrm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/smartrm/$version"
-    }
-    elseif ($product -eq "winthor") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/winthor/$version"
-    }
-    elseif ($product -eq "consinco") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/consinco/$version"
-    }
-    else {
-        Write-Host "Produto não reconhecido. Utilizando o produto genérico."
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
+    $repoURI = "http://192.168.1.125:8080/Hardening-OS/"
+    
+    # Cria o dicionário de dados utilizando JSON com os arquivos de configuração dos produtos
+    $json = '
+        {
+          "generic": [
+            {
+              "url": "generic/_var_version/hardening-Invoker-OS-Generic.ps1"
+            },
+            {
+              "url": "generic/_var_version/hardening-Config-OS-generic-w2k16.json"
+            },
+            {
+              "url": "generic/_var_version/hardening-Complementary-generic-AuditPol.ps1"
+            },
+            {
+              "url": "generic/_var_version/hardening-Complementary-generic-UnwantedSVCs.ps1"
+            }
+          ],
+          "datasul": [
+            {
+              "url": "generic/_var_version/hardening-Invoker-OS-Generic.ps1"
+            },
+            {
+              "url": "datasul/_var_version/hardening-Config-OS-datasul.json"
+            },
+            {
+              "url": "generic/_var_version/hardening-Complementary-generic-AuditPol.ps1"
+            },
+            {
+              "url": "generic/_var_version/hardening-Complementary-generic-UnwantedSVCs.ps1"
+            }
+          ]
+        }
+    '
+    $repository = $json | ConvertFrom-Json
+       
+
+    # Define o diretório de destino
+    $destPath = "c:\totvs\hardening\"
+    
+    # Cria o diretório de destino se ele não existir
+    if (!(Test-Path -Path $destPath -PathType Container)) {
+        New-Item -ItemType Directory -Path $destPath
     }
 
-    # Cria o dicionário de dados com os arquivos de configuração dos produtos
-    if ($product -eq "generic") {
-        $urls = @{
-            "generic" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-Generic-w2k16.json" = "$repoUrl/hardening-Config-OS-Generic-w2k16.json" },
-                @{ "hardening-Complementary-Generic-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Generic-AuditPol.ps1" },
-                @{ "hardening-Complementary-Generic-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Generic-UnwantedSVCs.ps1" }
-            )
-        }
+    # Permeia pelo json para captura da URI de cada arquivo do produto e realiza o download
+    foreach ($file in $repository.$product) {
+        $uri = [regex]::Replace($repoURI+$file.url,"_var_version",$version)
+        $fileName = $uri.Split("/")[-1]
+        $filePath = Join-Path -Path $destPath -ChildPath $fileName
+
+        write-host $uri
+                
+        Invoke-WebRequest $url -OutFile $filePath
     }
-    elseif ($product -eq "datasul") {
-        $urls = @{
-            "datasul" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-Datasul-w2k16.json" = "$repoUrl/hardening-Config-OS-Datasul-w2k16.json" },
-                @{ "hardening-Complementary-Datasul-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Datasul-AuditPol.ps1" },
-                @{ "hardening-Complementary-Datasul-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Datatasul-UnwantedSVCs.ps1" }
-            )
-        }    
-    } 
-    elseif ($product -eq "legaldesk") {
-        $urls = @{
-            "legaldesk" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-legaldesk-w2k16.json" = "$repoUrl/hardening-Config-OS-legldesk-w2k16.json" },
-                @{ "hardening-Complementary-legaldesk-AuditPol.ps1" = "$repoUrl/hardening-Complementary-legaldesk-AuditPol.ps1" },
-                @{ "hardening-Complementary-legaldesk-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Legaldesk-UnwantedSVCs.ps1" }
-            )
-        }    
-    }  
-    elseif ($product -eq "sisjuri") {
-        $urls = @{
-            "sisjuri" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-sisjuri-w2k16.json" = "$repoUrl/hardening-Config-OS-sisjuri-w2k16.json" },
-                @{ "hardening-Complementary-sisjuri-AuditPol.ps1" = "$repoUrl/hardening-Complementary-sisjuri-AuditPol.ps1" },
-                @{ "hardening-Complementary-sisjuri-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-sisjuri-UnwantedSVCs.ps1" }
-            )
-        }    
-    } 
-    elseif ($product -eq "rm") {
-        $urls = @{
-            "rm" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-rm-w2k16.json" = "$repoUrl/hardening-Config-OS-rm-w2k16.json" },
-                @{ "hardening-Complementary-rm-AuditPol.ps1" = "$repoUrl/hardening-Complementary-rm-AuditPol.ps1" },
-                @{ "hardening-Complementary-rm-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-rm-UnwantedSVCs.ps1" }
-            )
-        }    
-    }  
-    elseif ($product -eq "protheus") {
-        $urls = @{
-            "protheus" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-protheus-w2k16.json" = "$repoUrl/hardening-Config-OS-protheus-w2k16.json" },
-                @{ "hardening-Complementary-protheus-AuditPol.ps1" = "$repoUrl/hardening-Complementary-protheus-AuditPol.ps1" },
-                @{ "hardening-Complementary-protheus-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-protheus-UnwantedSVCs.ps1" }
-            )
-        }    
-    }
-    elseif ($product -eq "smartrm") {
-        $urls = @{
-            "smartrm" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-smatrm-w2k16.json" = "$repoUrl/hardening-Config-OS-smatrm-w2k16.json" },
-                @{ "hardening-Complementary-smartrm-AuditPol.ps1" = "$repoUrl/hardening-Complementary-smartrm-AuditPol.ps1" },
-                @{ "hardening-Complementary-smartrm-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-smartrm-UnwantedSVCs.ps1" }
-            )
-        }    
-    } 
-    elseif ($product -eq "winthor") {
-        $urls = @{
-            "winthor" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-winthor-w2k16.json" = "$repoUrl/hardening-Config-OS-winthor-w2k16.json" },
-                @{ "hardening-Complementary-winthor-AuditPol.ps1" = "$repoUrl/hardening-Complementary-winthor-AuditPol.ps1" },
-                @{ "hardening-Complementary-winthor-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-winthor-UnwantedSVCs.ps1" }
-            )
-        }    
-    }
-    elseif ($product -eq "consinco") {
-        $urls = @{
-            "consinco" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-consinco-w2k16.json" = "$repoUrl/hardening-Config-OS-consinco-w2k16.json" },
-                @{ "hardening-Complementary-consinco-AuditPol.ps1" = "$repoUrl/hardening-Complementary-consinco-AuditPol.ps1" },
-                @{ "hardening-Complementary-consinco-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-consinco-UnwantedSVCs.ps1" }
-            )
-        }    
-    }                                     
+}
+##Cria o agendamento do enforcement no task scheduler
+function enforcement-Hardening {
+    # Define o nome da tarefa agendada
+    $taskName = "Enforcement-Hardening" 
+
+    # Caminho do arquivo .ps1 e os parâmetros necessários
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"C:\totvs\hardening\hardening-Invoker-OS.ps1`" -apply hardening -configfile `"c:\totvs\hardening\cis_hardening_windows_server_2016_default.json`" -productfile `"c:\totvs\hardening\diff.json`"" 
+
+    # Define o gatilho para a tarefa agendada
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date.AddDays(1) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
+
+    # Cria a tarefa agendada
+    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "NT AUTHORITY\SYSTEM"
 }
 
-    # Verifica se o diretório local existe, caso não exista, cria a estrutura
-    if (!(Test-Path $localDirectory)) {
-        New-Item -ItemType Directory -Force -Path $localDirectory | Out-Null
-        Write-Host "Diretório $localDirectory criado com sucesso."
-    }
 
-    # Faz o download dos arquivos
-    foreach ($url in $urls.GetEnumerator()) {
-        $fileName = $url.Key
-        $fileUrl = $url.Value
-        $filePath = Join-Path -Path $localDirectory -ChildPath $fileName
 
-        Write-Host "Fazendo download do arquivo $fileName de $fileUrl para $filePath"
 
-        # Verifica se o arquivo já existe localmente, caso exista, pula o download
-        if (Test-Path $filePath) {
-            Write-Host "O arquivo $fileName já existe localmente, pulando o download."
-            continue
-        }
+#-------------------------------------------START SCRIPT----------------------------------------------------------
+#Incia a chamada das funções
 
-        try {
-            Invoke-WebRequest -Uri $fileUrl -OutFile $filePath -UseBasicParsing -ErrorAction Stop
-            Write-Host "Download do arquivo $fileName concluído com sucesso."
-        } catch {
-            Write-Host "Erro ao fazer o download do arquivo $fileName de $fileUrl. Detalhes do erro: $_"
-        }
-    }
+Admin-check
+
+download_hardening_files -Product "generic" -version "stable"
+
+enforcement-Hardening
+
+
