40d7966848822d2ef6a419678ba864f8f5ee66df
Antonio F Camelo <113397323+antoniocamello@users.noreply.github.com>
2023-05-03 09:10:05 -0300

    Update hardening-Applier.ps1

--- hardening-Applier.ps1
+++ hardening-Applier.ps1
@@ -1,101 +1,116 @@
-    param (
-        [string]$product = "generic",
-        [string]$version = "stable"
-    )
+param (
+    [string]$product = "generic",
+    [string]$version = "stable"
+)
 
-    # Define o diretório local onde os arquivos serão baixados
-    $localDirectory = "C:\totvs\hardening"
-    if (!(Test-Path $localDirectory)) {
-        New-Item -ItemType Directory -Path $localDirectory
+# Define o diretório local onde os arquivos serão baixados
+$localDirectory = "C:\totvs\hardening"
+if (!(Test-Path $localDirectory)) {
+    New-Item -ItemType Directory -Path $localDirectory
+}
+
+# Define a URL do repositório
+if ($product -eq "generic") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
+}
+elseif ($product -eq "datasul") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/datasul/$version"
+}
+elseif ($product -eq "legaldesk") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/legaldesk/$version"
+}
+elseif ($product -eq "sisjuri") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/sisjuri/$version"
+}
+elseif ($product -eq "protheus") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/protheus/$version"
+}
+elseif ($product -eq "rm") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/rm/$version"
+}
+elseif ($product -eq "smartrm") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/smartrm/$version"
+}
+elseif ($product -eq "winthor") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/winthor/$version"
+}
+elseif ($product -eq "consinco") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/consinco/$version"
+}
+else {
+    Write-Host "Produto não reconhecido. Utilizando o produto genérico."
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
+}
+
+
+# Cria o dicionário de dados com os arquivos de configuração dos produtos
+if ($product -eq "generic") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
+    $urls = @{
+        "generic" = @(
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
+            @{ "hardening-Config-OS-Generic-w2k16.json" = "$repoUrl/hardening-Config-OS-Generic-w2k16.json" },
+            @{ "hardening-Complementary-Generic-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Generic-AuditPol.ps1" },
+            @{ "hardening-Complementary-Generic-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Generic-UnwantedSVCs.ps1" }
+        )
     }
-
-    # Define a URL do repositório
-    if ($product -eq "generic") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
-    } elseif ($product -eq "datasul") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/datasul/$version"
-    } elseif ($product -eq "legaldesk") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/legaldesk/$version"
-    } elseif ($product -eq "sisjuri") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/sisjuri/$version"
-    } elseif ($product -eq "protheus") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/protheus/$version"
-    } elseif ($product -eq "rm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/rm/$version"
-    } elseif ($product -eq "smartrm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/smartrm/$version"
-    } elseif ($product -eq "winthor") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/winthor/$version"
-    } elseif ($product -eq "consinco") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/consinco/$version"
-    } else {
-        Write-Host "Produto não reconhecido. Utilizando o produto genérico."
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
+}
+elseif ($product -eq "datasul") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/datasul/$version"
+    $urls = @{
+        "datasul" = @(
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
+            @{ "hardening-Config-OS-Datasul-w2k16.json" = "$repoUrl/hardening-Config-OS-Datasul-w2k16.json" },
+            @{ "hardening-Complementary-Datasul-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Datasul-AuditPol.ps1" },
+            @{ "hardening-Complementary-Datasul-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Datasul-UnwantedSVCs.ps1" }
+        )
     }
-
-
-    # Cria o dicionário de dados com os arquivos de configuração dos produtos
-    if ($product -eq "generic") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/generic/$version"
-        $urls = @{
-            "generic" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-Generic-w2k16.json" = "$repoUrl/hardening-Config-OS-Generic-w2k16.json" },
-                @{ "hardening-Complementary-Generic-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Generic-AuditPol.ps1" },
-                @{ "hardening-Complementary-Generic-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Generic-UnwantedSVCs.ps1" }
-            )
-        }
-    } elseif ($product -eq "datasul") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/datasul/$version"
-        $urls = @{
-            "datasul" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Generic.ps1" },
-                @{ "hardening-Config-OS-Datasul-w2k16.json" = "$repoUrl/hardening-Config-OS-Datasul-w2k16.json" },
-                @{ "hardening-Complementary-Datasul-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Datasul-AuditPol.ps1" },
-                @{ "hardening-Complementary-Datasul-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Datasul-UnwantedSVCs.ps1" }
-            )
-        }
-    } elseif ($product -eq "legaldesk") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/legaldesk/$version"
-        $urls = @{
-            "legaldesk" = @(
-                @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Legaldesk.ps1" },
-                @{ "hardening-Config-OS-Legaldesk-w2k16.json" = "$repoUrl/hardening-Config-OS-Legaldesk-w2k16.json" },
-                @{ "hardening-Complementary-Legaldesk-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Legaldesk-AuditPol.ps1" },
-                @{ "hardening-Complementary-Legaldesk-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Legaldesk-UnwantedSVCs.ps1" }
-            )
-        }
+}
+elseif ($product -eq "legaldesk") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/legaldesk/$version"
+    $urls = @{
+        "legaldesk" = @(
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Legaldesk.ps1" },
+            @{ "hardening-Config-OS-Legaldesk-w2k16.json" = "$repoUrl/hardening-Config-OS-Legaldesk-w2k16.json" },
+            @{ "hardening-Complementary-Legaldesk-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Legaldesk-AuditPol.ps1" },
+            @{ "hardening-Complementary-Legaldesk-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Legaldesk-UnwantedSVCs.ps1" }
+        )
     }
-        } elseif ($product -eq "protheus") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/protheus/$version"
-        $urls = @{
+}
+
+} 
+elseif ($product -eq "protheus") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/protheus/$version"
+    $urls = @{
         "protheus" = @(
-        @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Protheus.ps1" },
-        @{ "hardening-Config-OS-Protheus-w2k16.json" = "$repoUrl/hardening-Config-OS-Protheus-w2k16.json" },
-        @{ "hardening-Complementary-Protheus-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Protheus-AuditPol.ps1" },
-        @{ "hardening-Complementary-Protheus-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Protheus-UnwantedSVCs.ps1" }
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-Protheus.ps1" },
+            @{ "hardening-Config-OS-Protheus-w2k16.json" = "$repoUrl/hardening-Config-OS-Protheus-w2k16.json" },
+            @{ "hardening-Complementary-Protheus-AuditPol.ps1" = "$repoUrl/hardening-Complementary-Protheus-AuditPol.ps1" },
+            @{ "hardening-Complementary-Protheus-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-Protheus-UnwantedSVCs.ps1" }
         )
-        }
-        } elseif ($product -eq "rm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/rm/$version"
-        $urls = @{
+    }
+} elseif ($product -eq "rm") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/rm/$version"
+    $urls = @{
         "rm" = @(
-        @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-RM.ps1" },
-        @{ "hardening-Config-OS-RM-w2k16.json" = "$repoUrl/hardening-Config-OS-RM-w2k16.json" },
-        @{ "hardening-Complementary-RM-AuditPol.ps1" = "$repoUrl/hardening-Complementary-RM-AuditPol.ps1" },
-        @{ "hardening-Complementary-RM-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-RM-UnwantedSVCs.ps1" }
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-RM.ps1" },
+            @{ "hardening-Config-OS-RM-w2k16.json" = "$repoUrl/hardening-Config-OS-RM-w2k16.json" },
+            @{ "hardening-Complementary-RM-AuditPol.ps1" = "$repoUrl/hardening-Complementary-RM-AuditPol.ps1" },
+            @{ "hardening-Complementary-RM-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-RM-UnwantedSVCs.ps1" }
         )
-        }
-        } elseif ($product -eq "smartrm") {
-        $repoUrl = "http://192.168.1.125:8080/Hardening-OS/smartrm/$version"
-        $urls = @{
+    }
+} elseif ($product -eq "smartrm") {
+    $repoUrl = "http://192.168.1.125:8080/Hardening-OS/smartrm/$version"
+    $urls = @{
         "smartrm" = @(
-        @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-SmartRM.ps1" },
-        @{ "hardening-Config-OS-SmartRM-w2k16.json" = "$repoUrl/hardening-Config-OS-SmartRM-w2k16.json" },
-        @{ "hardening-Complementary-SmartRM-AuditPol.ps1" = "$repoUrl/hardening-Complementary-SmartRM-AuditPol.ps1" },
-        @{ "hardening-Complementary-SmartRM-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-SmartRM-UnwantedSVCs.ps1" }
+            @{ "hardening-Invoker-OS.ps1" = "$repoUrl/hardening-Invoker-OS-SmartRM.ps1" },
+            @{ "hardening-Config-OS-SmartRM-w2k16.json" = "$repoUrl/hardening-Config-OS-SmartRM-w2k16.json" },
+            @{ "hardening-Complementary-SmartRM-AuditPol.ps1" = "$repoUrl/hardening-Complementary-SmartRM-AuditPol.ps1" },
+            @{ "hardening-Complementary-SmartRM-UnwantedSVCs.ps1" = "$repoUrl/hardening-Complementary-SmartRM-UnwantedSVCs.ps1" }
         )
     } 
+}
+
 
     # Verifica se o diretório local existe, caso não exista, cria a estrutura
     if (!(Test-Path $localDirectory)) {
